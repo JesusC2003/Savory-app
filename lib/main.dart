22f06b8513042bf6f0c3535edf9a9290dc9fc9a3
Jesus Castillo <jesusecastillo@unicesar.edu.co>
2025-10-26 16:01:08 -0500

    separación de la clase myapp del main al app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:proyecto_savory/UI/auth/login_page.dart';
+import 'package:proyecto_savory/UI/app.dart';
 import 'package:proyecto_savory/firebase_options.dart';
 
 
@@ -12,15 +12,4 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Savory App',
-      home: const LoginPage(),
-    );
-  }
-}
